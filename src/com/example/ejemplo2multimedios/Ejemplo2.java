4fcdbcbcfd8194cf258f71fa8f5be7c5ac1d9c4d
xkiver <xkiver@gmail.com>
2013-10-05 10:57:56 -0400

    Tercer commit

--- src/com/example/ejemplo2multimedios/Ejemplo2.java
+++ src/com/example/ejemplo2multimedios/Ejemplo2.java
@@ -12,6 +12,8 @@ public class Ejemplo2 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ejemplo2);
+		
+		i = 1;
 	}
 
 	@Override
